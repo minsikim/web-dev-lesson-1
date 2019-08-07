d65688befa9dd6561edaa422e37421dee769ebf5
Salinger MS <minsiua@gmail.com>
2019-08-08 04:32:16 +0900

    Lesson-02

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+var myNumber = 1;
+var myString = "Hello";
+var myBool = true;
+var myObject = { "first": 1, "second": 2 };
+var myArray = [1, 2, 3];
+var myFunction = function(){alert()};
+var myClass = new class { prop = "prop" };
+
+console.log("typeof myNumber: "+ typeof myNumber);
+console.log("typeof myString: "+ typeof myString);
+console.log("typeof myBool: "+ typeof myBool);
+console.log("typeof myObject: "+ typeof myObject);
+console.log("typeof myArray: "+ typeof myArray);
+console.log("typeof myFunction: "+ typeof myFunction);
+console.log("typeof myClass: "+ typeof myClass);
